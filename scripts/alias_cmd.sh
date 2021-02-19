3e12bedd3f8537ff238685b1bce5f91835d2bd1e
tomson784 <tomohito040759@gmail.com>
2021-02-19 11:52:55 +0900

    add alias check ros env command

--- scripts/alias_cmd.sh
+++ scripts/alias_cmd.sh
@@ -1,4 +1,7 @@
-# alias cm 
-cd ~/catkin_ws/ && catkin_make
-# alias cm_test
-cd ~/catkin_ws/ && catkin_make run_tests
+# alias: catkin_make
+alias cm='cd ~/catkin_ws/ && catkin_make'
+# alias: catkin_make run_tests
+alias cm_test='cd ~/catkin_ws/ && catkin_make run_tests'
+# alias: check the ros env
+alias rosenv='env | grep ROS'
+
