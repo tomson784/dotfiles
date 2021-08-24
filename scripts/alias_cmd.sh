870d9341ba3c837bcae04d2d2c53b2f3b53728f4
tomson784 <42789340+tomson784@users.noreply.github.com>
2021-08-24 22:20:38 +0900

    Update alias_cmd.sh

--- scripts/alias_cmd.sh
+++ scripts/alias_cmd.sh
@@ -1,7 +1,7 @@
 # alias: catkin_make
-alias cm='cd ~/catkin_ws/ && catkin_make'
+echo >> alias cm='cd ~/catkin_ws/ && catkin_make'
 # alias: catkin_make run_tests
-alias cm_test='cd ~/catkin_ws/ && catkin_make run_tests'
+echo >> alias cm_test='cd ~/catkin_ws/ && catkin_make run_tests'
 # alias: check the ros env
-alias rosenv='env | grep ROS'
+echo >> alias rosenv='env | grep ROS'
 
