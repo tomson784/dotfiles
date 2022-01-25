04b97d16e421c069a7dfed55ad42e87509fbb81a
tomson784 <tomohito040759@gmail.com>
2022-01-25 17:06:59 +0900

    up

--- scripts/alias_cmd.sh
+++ scripts/alias_cmd.sh
@@ -4,4 +4,5 @@ alias cm='cd ~/catkin_ws/ && catkin_make'
 alias cm_test='cd ~/catkin_ws/ && catkin_make run_tests'
 # alias: check the ros env
 alias rosenv='env | grep ROS'
-
+# arduino permissions
+alias ar='bash ~/dotfiles/scripts/arduino_permit.sh'
