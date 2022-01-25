e16844af75199937f9a58d64227c2c92984d2ce2
tomson784 <tomohito040759@gmail.com>
2022-01-25 17:10:14 +0900

    up

--- scripts/alias_cmd.sh
+++ scripts/alias_cmd.sh
@@ -1,13 +1,8 @@
 # alias: catkin_make
-echo >> alias cm='cd ~/catkin_ws/ && catkin_make'
+alias cm='cd ~/catkin_ws/ && catkin_make'
 # alias: catkin_make run_tests
-echo >> alias cm_test='cd ~/catkin_ws/ && catkin_make run_tests'
+alias cm_test='cd ~/catkin_ws/ && catkin_make run_tests'
 # alias: check the ros env
-<<<<<<< HEAD
 alias rosenv='env | grep ROS'
 # arduino permissions
 alias ar='bash ~/dotfiles/scripts/arduino_permit.sh'
-=======
-echo >> alias rosenv='env | grep ROS'
-
->>>>>>> 870d9341ba3c837bcae04d2d2c53b2f3b53728f4
