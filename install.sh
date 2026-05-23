da982ca28cc2d986c0ba0fcf152e5a54915252cb
tomson784 <tomohitokawabe@gmail.com>
2026-05-24 01:08:38 +0900

    update readme

--- install.sh
+++ install.sh
@@ -7,5 +7,10 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 echo "alias tmux_start='tmux new-session \; split-window -v \; split-window -h \; source-file ~/.tmux.conf\;'" >> ~/.bashrc
 # ターミナルの色を変えるための設定
 echo "export LS_COLORS='di=01;35'" >> ~/.bashrc
-# git branchをプロンプトに表示するための設定
-echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]$(__git_ps1)\n\$ '" >> ~/.bashrc
+# git branchをプロンプトに表示するための設定（元のPS1をコメントアウトして新しいものを追加）
+sed -i "s/^\(    \)\(PS1='.*01;34m.*'\)$/\1# \2/" ~/.bashrc
+cat >> ~/.bashrc << 'EOF'
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]$(__git_ps1)\n\$ '
+EOF
+# colcon buildの補完を有効にするための設定
+echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
